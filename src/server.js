d253b7dac60036f33d3ea618139e0d63ba63d992
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-12-28 19:47:05 -0300

    feat: set socket connection

--- src/server.js
+++ src/server.js
@@ -1,16 +1,26 @@
 import express from "express"
+import http from "http"
 import dotenv from "dotenv"
 import cors from "cors"
+import { Server } from "socket.io"
 import apiRoutes from "./routes/index.js"
 
 dotenv.config()
 
-const server = express()
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: "http://localhost:3000",
+})
 
-server.use(cors())
-server.use(express.urlencoded({ extended: true }))
-server.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-server.use("/", apiRoutes)
+io.on("connection", (socket) => {
+  console.log("Conexão detectada...")
+})
+
+app.use("/", apiRoutes)
 
 server.listen(process.env.PORT)
